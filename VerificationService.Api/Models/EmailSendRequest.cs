ce5bf772dc94ab6acff5ead63a86ac94da561ef9
EliasGrafstrom <elias.grafstrom@gmail.com>
2025-05-09 13:30:18 +0200

    Refactor project to use WebApi namespace and enhance services
    - Changed namespace from `VerificationService.Api` to `WebApi` across multiple files.
    - Added `SendVerificationRequest` and `VerifyVerificationRequest` classes.
    - Updated `EmailSendRequest` to replace `Body` with `PlainText`.
    - Implemented new `Send` method in `VerificationController` for sending verification codes.
    - Enhanced `VerificationService` with methods for sending codes and generating email messages using Azure Service Bus.
    - Updated `Program.cs` to register services and configure middleware.
    - Modified `.gitignore` to include additional configuration files.
    - Updated `WebApi.csproj` with new package references for Azure Service Bus and Newtonsoft.Json.
    - Adjusted logging settings in `appsettings.json` and `appsettings.Development.json`.
    - Updated solution file to reflect new project structure and name.

--- VerificationService.Api/Models/EmailSendRequest.cs
+++ VerificationService.Api/Models/EmailSendRequest.cs
@@ -1,10 +1,10 @@
-﻿namespace VerificationService.Api.Models;
+﻿namespace WebApi.Models;
 
 public class EmailSendRequest
 {
     //lista med mottagare
     public List<string> Recipients { get; set; } = null!;
     public string Subject { get; set; } = null!;
-    public string Body { get; set; } = null!;
+    public string PlainText { get; set; } = null!;
     public string Html { get; set; } = null!;  
 }
--- VerificationService.Api/Program.cs
+++ VerificationService.Api/Program.cs
@@ -1,21 +1,19 @@
+using Azure.Messaging.ServiceBus;
+using WebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSingleton(x => new ServiceBusClient(builder.Configuration["ASB:ConnectionString"]));
+builder.Services.AddTransient<VerificationService>();
+
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
+app.MapOpenApi();
 app.UseHttpsRedirection();
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
